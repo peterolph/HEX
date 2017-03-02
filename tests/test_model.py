6c759f20a463056cd417092c310c3b68f61a2779
peterolph <pete@lshift.net>
2017-03-02 16:33:27 +0000

    pull out test model setup into a fixture

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,14 @@
 
 from haive import model, hexes, types
 import random
+import pytest
 
-def assert_consistent(m):
+@pytest.fixture(autouse=True)
+def m():
+	m = model.Model()
+
+	yield m
+
 	for token, loc in m.tokens.items():
 		if loc is not None:
 			print(token, loc)
@@ -14,25 +20,20 @@ def test_can_create():
 def test_can_str():
 	assert str(model.Model()) is not None
 
-def test_move():
-	m = model.Model()
+def test_move(m):
 	token = random.choice(list(m.tokens))
 	m.move(token, hexes.centre)
 	assert m.tokens[token] == hexes.centre
-	assert_consistent(m)
 
-def test_move_to_cover():
-	m = model.Model()
+def test_move_to_cover(m):
 	token1, token2 = random.sample(list(m.tokens), 2)
 	m.move(token1, hexes.centre)
 	m.move(token2, hexes.centre)
 	assert m.tokens[token2] == hexes.centre
 	assert m.tokens[token1] == token2
 	assert m.trapped(token1) == True
-	assert_consistent(m)
 
-def test_cover_then_uncover():
-	m = model.Model()
+def test_cover_then_uncover(m):
 	token1, token2 = random.sample(list(m.tokens), 2)
 	m.move(token1, hexes.centre)
 	m.move(token2, hexes.centre)
@@ -40,10 +41,8 @@ def test_cover_then_uncover():
 	assert m.tokens[token2] == hexes.offsets[0]
 	assert m.tokens[token1] == hexes.centre
 	assert m.trapped(token1) == False
-	assert_consistent(m)
 
-def test_cannot_split_hive():
-	m = model.Model()
+def test_cannot_split_hive(m):
 	token1, token2, token3 = random.sample(list(m.tokens), 3)
 	m.move(token1, hexes.centre)
 	m.move(token2, hexes.offsets[0])
@@ -51,4 +50,3 @@ def test_cannot_split_hive():
 	assert m.trapped(token1) == True
 	assert m.trapped(token2) == False
 	assert m.trapped(token3) == False
-	assert_consistent(m)
