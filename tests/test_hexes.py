f14066a954b36133196662a77195ba67d90ca2e6
peterolph <pete@lshift.net>
2017-12-21 13:28:49 +0000

    add a couple of hexes tests

--- tests/test_hexes.py
+++ tests/test_hexes.py
@@ -7,6 +7,9 @@ def rotate_n_times(offset,dir,n):
         offset = hexes.rotate(offset,dir)
     return offset
 
+def sum(set_of_hexes):
+	return functools.reduce(hexes.add, set_of_hexes)
+
 def test_add():
     assert hexes.add((1,2,3),(4,5,6)) == (5,7,9)
 
@@ -34,4 +37,13 @@ def test_rotate_to_opposite():
         assert rotate_n_times(offset,hexes.right,3) == hexes.opposite(offset)
 
 def test_all_offsets():
-    assert functools.reduce(hexes.add, hexes.offsets) == hexes.centre
+    assert sum(hexes.offsets) == hexes.centre
+
+def test_neighbours():
+	assert hexes.neighbours(hexes.centre) == set(hexes.offsets)
+	test_hex = (2,5,7)
+	assert sum(hexes.neighbours(test_hex)) == hexes.mul(test_hex, 6)
+
+def test_merge():
+	some_hexes = hexes.neighbours((9,2,4))
+	assert hexes.merge((some_hexes, some_hexes)) == some_hexes
