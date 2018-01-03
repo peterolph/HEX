cfb516551d238fbb71cba192b95791a705d7f57a
peterolph <pete@lshift.net>
2018-01-03 13:59:29 +0000

    remove some tabs

--- tests/test_hexes.py
+++ tests/test_hexes.py
@@ -8,7 +8,7 @@ def rotate_n_times(dir,offset,n):
     return offset
 
 def sum(set_of_hexes):
-	return functools.reduce(hexes.add, set_of_hexes)
+    return functools.reduce(hexes.add, set_of_hexes)
 
 def test_add():
     assert hexes.add((1,2,3),(4,5,6)) == (5,7,9)
@@ -40,12 +40,12 @@ def test_all_offsets():
     assert sum(hexes.offsets) == hexes.centre
 
 def test_neighbours():
-	assert hexes.neighbours(hexes.centre) == set(hexes.offsets)
-	test_hex = (2,5,7)
-	assert sum(hexes.neighbours(test_hex)) == hexes.mul(test_hex, 6)
+    assert hexes.neighbours(hexes.centre) == set(hexes.offsets)
+    test_hex = (2,5,7)
+    assert sum(hexes.neighbours(test_hex)) == hexes.mul(test_hex, 6)
 
 def test_merge():
-	some_hexes = hexes.neighbours((9,2,4))
-	assert hexes.merge(()) == set()
-	assert hexes.merge((some_hexes,)) == some_hexes
-	assert hexes.merge((some_hexes, some_hexes)) == some_hexes
+    some_hexes = hexes.neighbours((9,2,4))
+    assert hexes.merge(()) == set()
+    assert hexes.merge((some_hexes,)) == some_hexes
+    assert hexes.merge((some_hexes, some_hexes)) == some_hexes
